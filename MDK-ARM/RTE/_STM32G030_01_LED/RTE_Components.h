a2e81ba4de705bdcd40586b5c54e94178dc93ead
FYlove1 <166298147+FYlove1@users.noreply.github.com>
2024-12-20 14:19:29 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/_STM32G030_01_LED/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32G030_01_LED' 
+ * Target:  'STM32G030_01_LED' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/STM32G030_01_LED/startup_stm32g030xx.d
@@ -0,0 +1,1 @@
+stm32g030_01_led\startup_stm32g030xx.o: startup_stm32g030xx.s
